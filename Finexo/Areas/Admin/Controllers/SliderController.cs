d6df12e49ead798a6a216c9d4ae27dfc7cf30f79
naliyev02 <naliyev02.44@gmail.com>
2023-10-22 19:16:29 +0400

    Added SliderCRUD

--- Finexo/Areas/Admin/Controllers/SliderController.cs
+++ Finexo/Areas/Admin/Controllers/SliderController.cs
@@ -23,6 +23,8 @@ namespace Finexo.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             var sliders = await _context.Sliders.ToListAsync();
+            int slidersCount = sliders.Count;
+            ViewBag.SlidersCount = slidersCount;
 
             List<SliderViewModel> slidersViewModels = new List<SliderViewModel>();
 
@@ -87,15 +89,117 @@ namespace Finexo.Areas.Admin.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public async Task<IActionResult> Detail(int id)
+        {
+            var slider = await _context.Sliders.FirstOrDefaultAsync(s => s.Id == id);
+            if (slider == null)
+                return NotFound();
+
+            var sliderViewModel = new SliderViewModel
+            {
+                Id = slider.Id,
+                Title = slider.Title,
+                Description = slider.Description
+            };
+            ViewBag.Image = slider.Image;
+            return View(sliderViewModel);
+        }
+
+
         public async Task<IActionResult> Update(int id)
         {
             var slider = await _context.Sliders.FirstOrDefaultAsync(s => s.Id == id);
             if (slider == null)
                 return NotFound();
 
-            return View();
+            SliderViewModel sliderViewModel = new()
+            {
+                Id = slider.Id,
+                Title = slider.Title,
+                Description = slider.Description,
+            };
+
+            return View(sliderViewModel);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id, SliderViewModel sliderViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            var slider = await _context.Sliders.FirstOrDefaultAsync(s => s.Id == id);
+            if (slider == null)
+                return NotFound();
+
+            if (sliderViewModel.Image != null)
+            {
+
+                if (!sliderViewModel.Image.ContentType.Contains("image/"))
+                {
+                    ModelState.AddModelError("Image", "File must be image");
+                    return View();
+                }
+
+
+                string path = Path.Combine(_webHostEnvironment.WebRootPath, "assets", "images", "website-images", slider.Image);
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+
+                string fileName = $"{Guid.NewGuid()}-{sliderViewModel.Image.FileName}";
+                var newPath = Path.Combine(_webHostEnvironment.WebRootPath, "assets", "images", "website-images", fileName);
+                using (FileStream stream = new FileStream(newPath, FileMode.Create))
+                {
+                    await sliderViewModel.Image.CopyToAsync(stream);
+                }
+
+                slider.Image = fileName;
+            }
+
+            slider.Title = sliderViewModel.Title;
+            slider.Description = sliderViewModel.Description;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var slider = _context.Sliders.FirstOrDefault(s => s.Id == id);
+            if (slider == null)
+                return NotFound();
+
+            var sliderViewModel = new SliderViewModel
+            {
+                Id = slider.Id,
+                Title = slider.Title,
+                Description = slider.Description
+            };
+            ViewBag.Image = slider.Image;
+
+            return View(sliderViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName(nameof(Delete))]
+        public async Task<IActionResult> DeleteSlider(int id)
+        {
+            var slider = await _context.Sliders.FirstOrDefaultAsync(s => s.Id == id);
+            if (slider == null)
+                return NotFound();
+
+            string path = Path.Combine(_webHostEnvironment.WebRootPath, "assets", "images", "website-images", slider.Image);
+
+            _context.Sliders.Remove(slider);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
 
 
     }
